190194f6c4b2cd3a943e7b60cee99361beadb60b
donarazhar <donarazhar@gmail.com>
2024-07-03 09:22:59 +0700

    1 - Membuat landing page

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,8 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', function () {
-    return view('homepage');
+    return view('index');
 });
-Route::get('/dashboard', function () {
-    return view('dashboard');
+Route::get('/homepage', function () {
+    return view('homepage');
 });
