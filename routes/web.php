7f65911b1698c250b57eda45ca64367069c02b6a
donarazhar <donarazhar@gmail.com>
2024-07-05 13:25:53 +0700

    6 - Rombak Semua

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,10 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('index-web');
+    return view('index');
 });
 Route::get('/blog', function () {
-    return view('mobile-app');
+    return view('blog');
 });
 Route::get('/tailsimple', function () {
     return view('tailsimple');
